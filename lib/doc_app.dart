e2d232ffce1c327b465476ebac534775cac300c6
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-07-09 02:04:50 +0300

    1. add projects files. 2. finish project structure. 3. finish onboarding screen.

---
+++ lib/doc_app.dart
@@ -0,0 +1,26 @@
+import 'package:doc_app/core/routing/app_router.dart';
+import 'package:doc_app/core/routing/routes.dart';
+import 'package:doc_app/core/theming/color_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DocApp extends StatelessWidget {
+  const DocApp({super.key, required this.appRouter});
+  final AppRouter appRouter;
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: ColorsManager.mainBlue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
+  }
+}
